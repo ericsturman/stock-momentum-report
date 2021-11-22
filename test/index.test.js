a4699fcd35f9cb53649229ddba53d8ba7d27a609
eric <eric.sturman@gmail.com>
2021-11-22 01:47:21 +0000

    ...

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,10 @@
 const {handler} = require("../lib/index")
+const AWS = require('aws-sdk-mock');
 
 describe("index", () => {
     test("function returns null", () => {
+        AWS.mock('S3', 'putObject', Buffer.from(require('fs').readFileSync('testFile.csv')));
+
         expect(handler()).toEqual(undefined)
     })
 })
